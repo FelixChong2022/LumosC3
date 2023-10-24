382161758e9a0c95a5bc6fd4e92344ecf25f686f
FelixChong2022 <105020676+FelixChong2022@users.noreply.github.com>
2023-10-23 22:55:19 -0500

    Alpha

--- app/src/main/java/com/example/lumoscoop3/Screen/MusicScreen.kt
+++ app/src/main/java/com/example/lumoscoop3/Screen/MusicScreen.kt
@@ -2,26 +2,31 @@ package com.example.lumoscoop3.Screen
 
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lumoscoop3.Model.MusicEvent
@@ -34,6 +39,23 @@ fun MusicScreen(
     onEvent: (MusicEvent) -> Unit
 ) {
     Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Row(
+                     horizontalArrangement = Arrangement.Center
+                )
+                {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    text = "Library"
+                )
+                }
+            }
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = {
                 onEvent(MusicEvent.ShowDialog)
@@ -44,7 +66,8 @@ fun MusicScreen(
                 )
             }
         },
-    ) { _ ->
+    )
+    { _ ->
         if (state.isAddingMusic) {
             AddMusicDialog(state = state, onEvent = onEvent)
         }
@@ -53,7 +76,8 @@ fun MusicScreen(
             contentPadding = PaddingValues(16.dp),
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
+        )
+        {
             item {
                 Row(
                     modifier = Modifier
@@ -85,9 +109,8 @@ fun MusicScreen(
                         )
                     }
                 }
-
             }
-
         }
-    }
+   }
 }
+
